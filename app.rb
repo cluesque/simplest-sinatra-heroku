6cdd93c36a5b556f0607390bf2c7183d90e005b9
Bill Kirtley <bill@spiffy.com>
2011-12-21 14:42:58 -0500

    Use slim for generating html output
    also, use an inline template

--- app.rb
+++ app.rb
@@ -1,4 +1,7 @@
 require 'sinatra'
+require 'slim'
+
+Slim::Engine.set_default_options :pretty => true
 
 get '/hi' do
   "Hello World\n"
@@ -7,3 +10,13 @@ end
 get '/page' do
   "<html><body><h1>Hello World!</h1></body></html>"
 end
+
+get '/template' do
+  slim :inline_template
+end
+__END__
+
+@@inline_template
+
+h1
+  | Hello World!!!
